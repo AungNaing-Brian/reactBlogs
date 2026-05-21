1063c379b68d69eba8f3c3eb5512557880d90849
Aung Naing <aungnaing.brian@gmail.com>
2026-05-21 14:54:45 +0400

    c8ep49

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 
+import { Outlet, NavLink } from 'react-router-dom';
 import './App.css';
 
 function App() {
@@ -7,11 +8,12 @@ function App() {
       <nav>
         <h1>My Blogs</h1>
         <ul>
-          <li><a href='#'>Home</a></li>
-          <li><a href='#'>About</a></li>
+          <li><NavLink to='/'>Home</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+          <li><NavLink to='/contact'>Contact</NavLink></li>
         </ul>
       </nav>
-
+      <Outlet/>
     </div>
   );
 }
