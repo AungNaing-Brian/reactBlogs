103b11f2f48c4fe1cb76f53762efa30c7e7ea064
Aung Naing <aungnaing.brian@gmail.com>
2026-05-21 22:52:23 +0400

    c8ep50

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,27 @@
+import useFetch from "../hooks/useFetch";
+import './Home.css';
+
 function Home(){
+
+    let url = 'http://localhost:3001/blogs';
+    let {data : blogs, loading , error} = useFetch(url);
     return (
-        <div>
-            <h1>Home</h1>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <div className="Home">
+            {error && <div>{error}</div>}
+            {loading && <div>Loading...</div>}
+            {blogs && blogs.map(blog=>(
+                <div key={blog.id} className="card">
+                    <h3>
+                        {blog.title}
+                    </h3>
+                    <p>
+                       posted by - {blog.author}
+                    </p>
+                    <a href="">
+                        Read More
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
